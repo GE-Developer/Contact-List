7910602980ac6d65e1fe55320a00699ef61236ea
Michael Bukhrashvili <on-line-misha@mail.ru>
2021-06-17 02:38:02 +0300

    Initial Commit

---
+++ Contact List/Contact_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_ListApp.swift
+//  Contact List
+//
+//  Created by Mikhail Bukhrashvili on 17.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Contact_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contact List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Contact List
+//
+//  Created by Mikhail Bukhrashvili on 17.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
